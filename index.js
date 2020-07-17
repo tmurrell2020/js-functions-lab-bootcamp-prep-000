5ef6b9fdf64ca0b18459abaa1ad462a81ac3bbed
Terrell Murrell <littlerell12@gmail.com>
2020-07-17 15:52:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function happyHolidays(string)  {
   return "Happy holidays!";
 }
 
-function happyHolidaysTo("you")  {
+function happyHolidaysTo("name")  {
   return (`Happy holidays, ${name}!`);
 
 }
